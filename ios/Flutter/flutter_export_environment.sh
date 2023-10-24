9c95834671b45b1e4bd3e0fc4e8bce3a71b2fcde
Tilio <tilrobe@gmail.com>
2023-10-24 11:39:44 +0200

    Build project + Firebase

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/tebor/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/tebor/Fac/MMM/mmm_project"
+export "FLUTTER_APPLICATION_PATH=/home/tebor/Fac/Git/MMM-Project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
