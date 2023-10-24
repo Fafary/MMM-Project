6ab296c4a20904835418c8032237e86d98b4ad33
18000328 <alexandre.chaplais@etudiant.univ-rennes1.fr>
2023-10-24 14:10:21 +0200

    creation login_screen.dart

---
+++ lib/login_screen.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+
+class LoginScreenWidget extends StatefulWidget {
+  const LoginScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenWidgetState createState() => _LoginScreenWidgetState();
+}
+
+class _LoginScreenWidgetState extends State<LoginScreenWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  late FocusNode emailFocusNode;
+  late FocusNode passwordFocusNode;
+  bool passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    emailFocusNode = FocusNode();
+    passwordFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (emailFocusNode.hasFocus || passwordFocusNode.hasFocus) {
+          FocusScope.of(context).unfocus();
+        }
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: Color(0xFFF5F5F5),
+        body: SafeArea(
+          top: true,
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 32.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Biodiversity App',
+                      style: TextStyle(
+                        //fontFamily: 'YourFont',
+                        color: Colors.black,
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF5F5F5),
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Login or Register',
+                            style: TextStyle(
+                              fontFamily: 'YourFont',
+                              color: Colors.black,
+                              fontSize: 34,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12.0),
+                            child: Text(
+                              'Welcome to the Biodiversity App. Please login or register to continue.',
+                              style: TextStyle(
+                                fontFamily: 'YourFont',
+                                color: Colors.black,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          TextFormField(
+                            controller: emailController,
+                            focusNode: emailFocusNode,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              hintStyle: TextStyle(
+                                fontFamily: 'YourFont',
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.transparent,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                            ),
+                            style: TextStyle(
+                              fontFamily: 'YourFont',
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                            child: TextFormField(
+                              controller: passwordController,
+                              focusNode: passwordFocusNode,
+                              obscureText: !passwordVisible,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                hintStyle: TextStyle(
+                                  fontFamily: 'YourFont',
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.transparent,
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                suffixIcon: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      passwordVisible = !passwordVisible;
+                                    });
+                                  },
+                                  child: Icon(
+                                    passwordVisible
+                                        ? Icons.visibility_outlined
+                                        : Icons.visibility_off_outlined,
+                                    color: Color(0xFF757575),
+                                    size: 22,
+                                  ),
+                                ),
+                              ),
+                              style: TextStyle(
+                                fontFamily: 'YourFont',
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              print('Button pressed ...');
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontFamily: 'YourFont',
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                              elevation: 3,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't have an account?",
+                                style: TextStyle(
+                                  fontFamily: 'YourFont',
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                'Register Here',
+                                style: TextStyle(
+                                  fontFamily: 'YourFont',
+                                  color: Colors.green,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'fiche_screen.dart';
 import 'home_screen.dart';
+import 'login_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/fiches': (BuildContext context) => const MyHomePage(),
-  '/': (BuildContext context) => const FicheScreen(),
+  '/fiche': (BuildContext context) => const FicheScreen(),
+  '/': (BuildContext context) => const LoginScreenWidget(),
 };
